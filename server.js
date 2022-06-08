6c1e51826379df6654064985a19e4319f3385f11
Bhargav Das Gupta <dasguptabhargav@gmail.com>
2022-06-09 00:18:47 +0530

    connect to atlas mongodb

--- server.js
+++ server.js
@@ -1,9 +1,33 @@
-var express = require('express');
+const express = require('express');
+const mongoose = require('mongoose');
 
-var app = express();
+const app = express();
 
-var server = app.listen(3000, () => {
+// mongodb atlas connection
+const dbUrl = 'mongodb+srv://bhargav:iWjp2xeF3TB9pfV@cluster0.jrxds.mongodb.net/?retryWrites=true&w=majority';
+
+mongoose.connect(dbUrl, (err) => {
+
+  if (err) {
+    console.log("unable to connect :(");
+    console.log("err:", err);
+  }
+  else
+    console.log("connection established...");
+});
+
+// create server and listen on port
+const server = app.listen(3000, () => {
   console.log("server is running on port", server.address().port);
 });
+
+// defining schema
+const messageSchema = {
+  name: String,
+  text: String
+};
+
+// compiling model
+const messageModel = ('Message', messageSchema);
 
 app.use(express.static(__dirname));
