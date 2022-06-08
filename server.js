d3075bae7a2606a32fffe58bc82eb37a94ff631a
Bhargav Das Gupta <dasguptabhargav@gmail.com>
2022-06-09 00:30:20 +0530

    bug fix
    don't create server if connection to db fails

--- server.js
+++ server.js
@@ -3,22 +3,26 @@ const mongoose = require('mongoose');
 
 const app = express();
 
-// mongodb atlas connection
+// mongodb atlas connection and server creation
 const dbUrl = 'mongodb+srv://bhargav:iWjp2xeF3TB9pfV@cluster0.jrxds.mongodb.net/?retryWrites=true&w=majority';
+
+let server;
 
 mongoose.connect(dbUrl, (err) => {
 
   if (err) {
-    console.log("unable to connect :(");
-    console.log("err:", err);
+      console.log("unable to connect to db :(");
+      console.log("Exiting...");
+      console.log("err:", err);
   }
-  else
-    console.log("connection established...");
-});
+  else {
+      console.log("db connection established...");
 
-// create server and listen on port
-const server = app.listen(3000, () => {
-  console.log("server is running on port", server.address().port);
+      // create server and listen on port
+      server = app.listen(3000, () => {
+        console.log("server is running on port", server.address().port);
+      });
+  }
 });
 
 // defining schema
