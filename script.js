6fbf0ae8920d26775705ce7f15330d5f7a0263f3
Bhargav Das Gupta <dasguptabhargav@gmail.com>
2022-06-21 22:34:36 +0530

    major bug fixes
    chat display fixed, displaying order reversed

--- script.js
+++ script.js
@@ -11,24 +11,24 @@ $(() => {
 
     $('#name').val("");
     $('#message').val("");
-
-    getMessages();
   });
 
-  function addMessage(message) {
-    $('#message').append(`
-        <h4> ${message.name} </h4>
-        <p> ${message.text} </p>
-    `);
-  }
-
-  function getMessages() {
-    $.get('http://localhost:3000/messages', (data) => {
-      data.forEach(addMessage);
-    });
-  }
-
-  function sendMessage(message) {
-    $.post('http://localhost:3000/messages', message);
-  }
+  getMessages();
 });
+
+function addMessage(message) {
+  $('#messages').prepend(`
+      <h4> ${message.name} </h4>
+      <p> ${message.text} </p>
+  `);
+}
+
+function getMessages() {
+  $.get('http://localhost:3000/messages', (data) => {
+    data.forEach(addMessage);
+  });
+}
+
+function sendMessage(message) {
+  $.post('http://localhost:3000/messages', message);
+}
